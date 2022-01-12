6cbf8099b75d0b83c125c3356b385863af89133a
Rakib-RS <rakib.rscse@gmail.com>
2022-01-12 20:29:59 +0600

    Update dbconfig.php

--- dbconfig.php
+++ dbconfig.php
@@ -1,6 +1,6 @@
 <?php
-    $host = 'localhost';
-    $dbname = 'information';
-    $username = 'root';
-    $password = '';
+    $host = 'sql6.freesqldatabase.com';
+    $dbname = 'sql6465167';
+    $username = 'sql6465167';
+    $password = 'GVZQvSvIe8';
 ?>
